c6e463f49d1abbb063150ee5a821523239b0ede2
Manny <manuelpo.msn.com>
2023-12-17 09:17:15 -0700

    all done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+saturdayFun();
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage:
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer"));
